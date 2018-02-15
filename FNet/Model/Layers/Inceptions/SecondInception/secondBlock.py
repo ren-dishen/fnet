32c54b11b76ca509bb4cbd90ee842df5fe5fd444
ren-dishen <qwerty-online@e-mail.ru>
2018-02-15 16:49:51 +0200

    Reinit commit

---
+++ FNet/Model/Layers/Inceptions/SecondInception/secondBlock.py
@@ -0,0 +1,32 @@
+from FNet.Model import blockFactory as factory
+
+def block3x3(input):
+    tensor = factory.convolutionBlock(input, 'IBlock/2/2/3x3/1', 160, (1,1))
+    tensor = factory.zeroPadding(tensor)
+    tensor = factory.convolutionBlock(tensor, 'IBlock/2/2/3x3/2', 256, (3,3), (2,2))
+
+    return tensor
+
+def block5x5(input):
+    tensor = factory.convolutionBlock(input, 'IBlock/2/2/5x5/1', 64, (1,1))
+    tensor = factory.zeroPadding(tensor, (2,2))
+    tensor = factory.convolutionBlock(tensor, 'IBlock/2/2/5x5/2', 128, (5,5), (2,2))
+
+    return tensor
+
+def blockPool(input):
+    tensor = factory.averagePooling(input, (3,3), (2,2))
+    tensor = factory.zeroPadding(tensor, (0,1), (0,1))
+
+    return tensor
+
+def inceptionConstructor(input):
+    _3x3 = block3x3(input)
+    _5x5 = block5x5(input)
+    _pool = blockPool(input)
+
+    tensor = factory.concatenate([_3x3, _5x5, _pool])
+
+    return tensor
+
+    
---
+++ FNet/Model/Layers/Inceptions/ThirdInception/firstBlock.py
@@ -0,0 +1,31 @@
+from FNet.Model import blockFactory as factory
+
+def block1x1(input):
+    tensor = factory.convolutionBlock(input, 'IBlock/3/1/1x1/1', 256, (1,1))
+
+    return tensor
+
+def block3x3(input):
+    tensor = factory.convolutionBlock(input, 'IBlock/3/1/3x3/1', 96, (1,1))
+    tensor = factory.zeroPadding(tensor)
+    tensor = factory.convolutionBlock(tensor, 'IBlock/3/1/3x3/2', 384, (3,3))
+
+    return tensor
+
+def blockPool(input):
+    tensor = factory.averagePooling(input, (3,3), (3,3))
+    tensor = factory.convolutionBlock(tensor, 'IBlock/3/1/pool/1', 96, (1,1))
+    tensor = factory.zeroPadding(tensor, (1,1))
+
+    return tensor
+
+def inceptionConstructor(input):
+    _3x3 = block3x3(input)
+    _pool = blockPool(input)
+    _1x1 = block1x1(input)
+
+    tensor = factory.concatenate([_3x3, _pool, _1x1])
+
+    return tensor
+
+    
